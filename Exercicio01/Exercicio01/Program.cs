1d2eac5c327e231148adcb572fb7ba73926e7d4f
vsoares99 <victor.o.soares@hotmail.com>
2024-09-02 14:56:50 -0300

    First commit

---
+++ Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,17 @@
+﻿using Exercicio01;
+using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+
+        }
+    }
+}
---
+++ Exercicio01/Exercicio01/Retangulo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+
+    }
+}

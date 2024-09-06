4f2863ba645bb873a2797108f02a7a7a1a33c172
vsoares99 <victor.o.soares@hotmail.com>
2024-09-05 23:16:52 -0300

    Exercicio 04

---
+++ Exercicio04/Produto.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+namespace Exercicio04
+{
+    internal class Produto
+    {
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto(string nome, double preco)
+        {
+            _nome = nome;
+            Preco = preco;
+        }
+
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            Quantidade = quantidade;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"{_nome}, " +
+                $"$ {Preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
+                $"{Quantidade} unidades, Total: $ {ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}

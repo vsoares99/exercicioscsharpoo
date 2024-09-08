ab858267b58438f56855431cd16c0aa4c164825f
vsoares99 <victor.o.soares@hotmail.com>
2024-09-08 17:39:04 -0300

    Exercício 06 Resolvido

---
+++ Exercicio06/Exercicio06/Program.cs
@@ -0,0 +1,40 @@
+﻿using Exercicio06;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vect = new Estudante[10];
+
+            Console.Write("Quantos quartos vão ser alugados? ");
+            int quartos = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= quartos; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel: {i}");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Nome: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudante(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine($"{i}: {vect[i]}");
+
+                }
+            }
+        }
+    }
+}

a98805de3443fae2c1ccf1f8b2010c8b24765013
vsoares99 <victor.o.soares@hotmail.com>
2024-09-18 22:20:27 -0300

    Exercicio 12 Resolvido

---
+++ Exercicio12/Exercicio12/Entities/ImportedProduct.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio12.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() { }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return $"{Name} $ {TotalPrice()} (Customs fee: $ {CustomsFee.ToString("F2", CultureInfo.InvariantCulture)})";
+        }
+    }
+}
